ae6ccf29a841f8b686852ea0c921fe5f9c80972b
Azryel <AzryelShroud@gmail.com>
2021-03-21 19:47:22 +0100

    update carController with skeleton

---
+++ controllers/carController.js
@@ -0,0 +1,41 @@
+const Car = require("../models/car");
+
+// Display car list
+exports.car_list = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author list');
+};
+
+// Display details of a car
+exports.car_detail = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Display car create form on GET
+exports.car_create_get = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Handle car create on POST
+exports.car_create_post = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Display car delete form on GET
+exports.car_delete_get = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Handle car delete on POST
+exports.car_delete_post = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Display car update form on GET
+exports.car_update_get = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+}
+
+// Handle car update on POST
+exports.car_update_post = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+};
