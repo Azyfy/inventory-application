250491b097e2d66af8b4cb22fbfb83f7cf54fcb4
Azryel <AzryelShroud@gmail.com>
2021-03-22 20:48:11 +0100

    update car routes

--- routes/inventory.js
+++ routes/inventory.js
@@ -9,6 +9,22 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Test!' });
   });
 
-router.get('/cars', car_controller.car_list);
+// get car list
+router.get("/cars", car_controller.car_list);
+
+// get car details
+router.get("/car/:id", car_controller.car_detail);
+
+// get and post car create form
+router.get("/car/create", car_controller.car_create_get);
+router.post("/car/create", car_controller.car_create_post);
+
+// get and post car delete
+router.get("/car/:id/delete", car_controller.car_delete_get);
+router.post("/car/:id/delete", car_controller.car_delete_post);
+
+// get and post car update
+router.get("/car/:id/update", car_controller.car_update_get);
+router.post("/car/:id/update", car_controller.car_update_post);
 
 module.exports = router;
