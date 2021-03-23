95be48d371295b553b41e44294ca9689d2b99ac5
Azryel <AzryelShroud@gmail.com>
2021-03-23 17:28:25 +0100

    update carsinstockController and routes with skeleton

---
+++ controllers/carinstockController.js
@@ -0,0 +1,45 @@
+const Car = require("../models/car");
+const CarsInStock = require("../models/carinstock");
+
+const async = require("async");
+
+
+// Display car list
+exports.carsinstock_list = (req, res, next) => {
+
+};
+
+// Display details of a car
+exports.carsinstock_detail = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Display car create form on GET
+exports.carsinstock_create_get = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Handle car create on POST
+exports.carsinstock_create_post = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Display car delete form on GET
+exports.carsinstock_delete_get = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Handle car delete on POST
+exports.carsinstock_delete_post = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Display car update form on GET
+exports.carsinstock_update_get = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+}
+
+// Handle car update on POST
+exports.carsinstock_update_post = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+};
--- routes/inventory.js
+++ routes/inventory.js
@@ -3,10 +3,12 @@ var router = express.Router();
 
 // controller models
 const car_controller = require("../controllers/carController");
+const carsinstock_controller = require("../controllers/carinstockController");
 
 
 router.get('/',  car_controller.inventory_index);
 
+// CAR ROUTES
 // get car list
 router.get("/car_list", car_controller.car_list);
 
@@ -24,5 +26,33 @@ router.post("/car/:id/delete", car_controller.car_delete_post);
 // get and post car update
 router.get("/car/:id/update", car_controller.car_update_get);
 router.post("/car/:id/update", car_controller.car_update_post);
+
+
+// CARSINSTOCK ROUTES
+// get carsinstock list
+router.get("/carsinstock_list", carsinstock_controller.carsinstock_list);
+
+// get carsinstock details
+router.get("/carsinstock/:id", carsinstock_controller.carsinstock_detail);
+
+// get and post carsinstock create form
+router.get("/carsinstock/create", carsinstock_controller.carsinstock_create_get);
+router.post("/carsinstock/create", carsinstock_controller.carsinstock_create_post);
+
+// get and post carsinstock delete
+router.get("/carsinstock/:id/delete", carsinstock_controller.carsinstock_delete_get);
+router.post("/carsinstock/:id/delete", carsinstock_controller.carsinstock_delete_post);
+
+// get and post carsinstock update
+router.get("/carsinstock/:id/update", carsinstock_controller.carsinstock_update_get);
+router.post("/carsinstock/:id/update", carsinstock_controller.carsinstock_update_post);
+
+
+
+
+
+
+
+
 
 module.exports = router;
