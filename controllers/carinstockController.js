28ff5d58761e1678946d66ac968b954325e1fb9a
Azryel <AzryelShroud@gmail.com>
2021-03-23 17:29:52 +0100

    change comments car to carsinstock

--- controllers/carinstockController.js
+++ controllers/carinstockController.js
@@ -4,42 +4,42 @@ const CarsInStock = require("../models/carinstock");
 const async = require("async");
 
 
-// Display car list
+// Display carsinstock list
 exports.carsinstock_list = (req, res, next) => {
 
 };
 
-// Display details of a car
+// Display details of carsinstock
 exports.carsinstock_detail = (req, res) => {
     res.send('NOT IMPLEMENTED');
 };
 
-// Display car create form on GET
+// Display carsinstock create form on GET
 exports.carsinstock_create_get = (req, res) => {
     res.send('NOT IMPLEMENTED');
 };
 
-// Handle car create on POST
+// Handle carsinstock create on POST
 exports.carsinstock_create_post = (req, res) => {
     res.send('NOT IMPLEMENTED');
 };
 
-// Display car delete form on GET
+// Display carsinstock delete form on GET
 exports.carsinstock_delete_get = (req, res) => {
     res.send('NOT IMPLEMENTED');
 };
 
-// Handle car delete on POST
+// Handle carsinstock delete on POST
 exports.carsinstock_delete_post = (req, res) => {
     res.send('NOT IMPLEMENTED');
 };
 
-// Display car update form on GET
+// Display carsinstock update form on GET
 exports.carsinstock_update_get = (req, res) => {
     res.send('NOT IMPLEMENTED');
 }
 
-// Handle car update on POST
+// Handle carsinstock update on POST
 exports.carsinstock_update_post = (req, res) => {
     res.send('NOT IMPLEMENTED');
 };
