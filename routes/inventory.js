a6f5a0d8a682fd1a79b6d868a2292319f1079f60
Azryel <AzryelShroud@gmail.com>
2021-03-25 20:32:26 +0100

    change reoute order for all

--- routes/inventory.js
+++ routes/inventory.js
@@ -13,12 +13,12 @@ router.get('/',  car_controller.inventory_index);
 // get car list
 router.get("/car_list", car_controller.car_list);
 
-// get car details
-router.get("/car/:id", car_controller.car_detail);
-
 // get and post car create form
 router.get("/car/create", car_controller.car_create_get);
 router.post("/car/create", car_controller.car_create_post);
+
+// get car details
+router.get("/car/:id", car_controller.car_detail);
 
 // get and post car delete
 router.get("/car/:id/delete", car_controller.car_delete_get);
@@ -33,12 +33,12 @@ router.post("/car/:id/update", car_controller.car_update_post);
 // get carsinstock list
 router.get("/carsinstock_list", carsinstock_controller.carsinstock_list);
 
-// get carsinstock details
-router.get("/carsinstock/:id", carsinstock_controller.carsinstock_detail);
-
 // get and post carsinstock create form
 router.get("/carsinstock/create", carsinstock_controller.carsinstock_create_get);
 router.post("/carsinstock/create", carsinstock_controller.carsinstock_create_post);
+
+// get carsinstock details
+router.get("/carsinstock/:id", carsinstock_controller.carsinstock_detail);
 
 // get and post carsinstock delete
 router.get("/carsinstock/:id/delete", carsinstock_controller.carsinstock_delete_get);
@@ -73,12 +73,12 @@ router.post("/category/:id/update", category_controller.category_update_post);
 // get manufacturer list
 router.get("/manufacturer_list", manufacturer_controller.manufacturer_list);
 
-// get manufacturer details
-router.get("/manufacturer/:id", manufacturer_controller.manufacturer_detail);
-
 // get and post manufacturer create form
 router.get("/manufacturer/create", manufacturer_controller.manufacturer_create_get);
 router.post("/manufacturer/create", manufacturer_controller.manufacturer_create_post);
+
+// get manufacturer details
+router.get("/manufacturer/:id", manufacturer_controller.manufacturer_detail);
 
 // get and post manufacturer delete
 router.get("/manufacturer/:id/delete", manufacturer_controller.manufacturer_delete_get);
