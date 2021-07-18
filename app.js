86deca5d16d6174dc424d97b9ee3c40a48ed8f5d
Azryel <AzryelShroud@gmail.com>
2021-07-18 22:46:54 +0200

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -8,12 +8,18 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const inventoryRouter = require("./routes/inventory");
 
+const compression = require('compression');
+const helmet = require('helmet');
+
 var app = express();
+
+app.use(helmet());
 
 
 //connect to mongodb
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://Azryel:mongo0atlas@cluster0.hf91u.mongodb.net/inventory_cars?retryWrites=true&w=majority";
+const dev_db_url = "mongodb+srv://Azryel:mongo0atlas@cluster0.hf91u.mongodb.net/inventory_cars?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error;"));
@@ -22,6 +28,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression()); 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
